c65cb22418962dded3e214025c283a8702836d70
Unknown <perryellis88@gmail.com>
2018-03-25 17:52:58 -0500

    Will be starting the Invoice page next

---
+++ CustomerManagement/CustomerManagement/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerManagement.Startup))]
+namespace CustomerManagement
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
